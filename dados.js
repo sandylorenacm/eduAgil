023e4b8e0802ad2cebfc54ddffa4cd04a69d4e2f
Sandy Monteiro <132113296+sandylorenacm@users.noreply.github.com>
2024-09-08 21:37:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,61 @@
+// Array utilizado para armazenar as informações  sobre cada metodologia, métodos, conceitos, e outras informações relevantes sobre agilidade em objetos na lista
+
+let dados = [
+ 
+    // Objeto representando o framework Scrum
+    {
+        titulo: "Scrum",
+        descricao: "Metodologia ágil que organiza o trabalho em ciclos curtos (sprints), com foco em entregas incrementais e adaptação contínua.",
+        link: "https://pt.wikipedia.org/wiki/Scrum",
+        palavrasChave: "scrum agile metodologia desenvolvimento sprint entregas incrementais adaptação contínua"
+    },
+ 
+    // Objeto representando o método Kanban
+    {
+        titulo: "Kanban",
+        descricao: "Método ágil que utiliza um sistema visual para gerenciar o fluxo de trabalho de forma eficiente. Ele permite visualizar o progresso das tarefas, identificar gargalos e otimizar a entrega contínua de valor.",
+        link: "https://pt.wikipedia.org/wiki/Kanban_(método)",
+        palavrasChave: "kanban agile método visual fluxo de trabalho tarefas gargalos entrega contínua"
+    },
+
+     // Objeto representando a técnica de PBB
+    {
+        titulo: "PBB (Product Backlog Building)",
+        descricao: "O Product Backlog Building (PBB) é uma técnica ágil essencial para criar um backlog detalhado e priorizado para seus projetos. Descubra como aumentar a colaboração e entregar mais valor com o PBB.",
+        link: "https://www.productbacklogbuilding.com/",
+        palavrasChave: "PBB Scrum Product Backlog detalhamento priorização"
+    },
+
+     // Objeto representando a filosofia Agile
+    {
+        titulo: "Agile",
+        descricao: "Filosofia de desenvolvimento de software que valoriza indivíduos e interações, software funcionando e colaboração com o cliente",
+        link: "https://agilemanifesto.org/iso/ptbr/manifesto.html",
+        palavrasChave: "agile manifesto minterativo incremental valor"
+    },
+
+// Objeto representando a metodologia XP
+    {
+        titulo: "XP (Extreme Programming)",
+        descricao: "É uma metodologia ágil de desenvolvimento de software que leva práticas de engenharia de software a um extremo para produzir software de alta qualidade de forma rápida e eficiente.",
+        link: "https://pt.wikipedia.org/wiki/Programação_extrema",
+        palavrasChave: "XP Extreme Programming metodologia ágil desenvolvimento de software práticas de engenharia alta qualidade rápido eficiente"
+    },
+
+// Objeto representando a metodologia Design Sprint
+    {
+        titulo: "Design Sprint",
+        descricao: "Metodologia para resolver problemas e testar ideias em apenas cinco dias, combinando design thinking e desenvolvimento ágil.",
+        link: "https://caroli.org/o-que-e-design-sprint/",
+        palavrasChave: "Design Sprint metodologia design thinking desenvolvimento ágil prototipagem teste de ideias cinco dias"
+    },
+
+    // Objeto representando a abordagem Design Thinking
+    {
+        titulo: "Design Thinking",
+        descricao: "Abordagem centrada no ser humano para a inovação, que busca entender as necessidades dos usuários e criar soluções criativas.",
+        link: "https://www.cursospm3.com.br/blog/design-thinking-guia-o-que-e-etapas-como-aplicar/",
+        palavrasChave: "Design Thinking inovação centrado usuário necessidades soluções criativas empatia ideação prototipação teste"
+    }
+
+];
